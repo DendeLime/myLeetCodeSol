db612e7530554871acb848d988b15d84ee78500e
DendeLime <Denverg52@hotmail.com>
2023-08-03 18:35:15 -0600

    1

--- Python/1 Two Sum.py
+++ Python/1 Two Sum.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/two-sum/
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        _dict = {}
+        for i in range(len(nums)):
+            ans = target - nums[i]
+            if ans in _dict:
+                return [_dict[ans], i]
+            _dict[nums[i]] = i
