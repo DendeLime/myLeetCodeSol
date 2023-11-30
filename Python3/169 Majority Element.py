a73bb4ba90d3ce827d2242a91ea73ce7ed7e14f2
DendeLime <Denverg52@hotmail.com>
2023-11-30 12:19:09 -0700

    169 Python3

---
+++ Python3/169 Majority Element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        # create a dictionary of nums and count the occurences
+        # return key with max value in dictionary
+        count = {}
+        length = len(nums)
+
+        for i in range(length):
+            if nums[i] in count:
+                count[nums[i]] += 1
+            else:
+                count[nums[i]] = 1
+
+        value = 0
+        for i in count:
+            if value == 0 or value < count[i]:
+                value = count[i]
+                ans = i
+
+        return ans
+        
